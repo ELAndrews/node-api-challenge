ffd322fb926bd5227c6f65b407fb4043f9351b85
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-17 09:37:10 +0000

    Add put to projects

--- data/routers/project-routers.js
+++ data/routers/project-routers.js
@@ -50,6 +50,34 @@ router.delete("/api/projects/:id", (req, res) => {
     });
 });
 
-router.put("/", (req, res) => {});
+router.put("/api/projects", (req, res) => {
+  if (req.body.id && req.body.name && req.body.description) {
+    projects
+      .get(req.body.id)
+      .then(post => {
+        if (post === null) {
+          res.status(404).json(`There isn't a project with this id`);
+        } else {
+          projects
+            .update(req.body.id, req.body)
+            .then(updated => {
+              res.status(202).json(updated);
+            })
+            .catch(error => {
+              res.status(500).json(`There was an error updating this post`);
+            });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(`There was an error collecting data`);
+      });
+  } else {
+    res
+      .status(400)
+      .json(
+        "Please provide a post id, and the changes, including: a name and a description for your post"
+      );
+  }
+});
 
 module.exports = router;
