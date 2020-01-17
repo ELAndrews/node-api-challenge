9483840b5df4137fc6aa79b09d5436b5f0125b93
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-01-17 10:10:33 +0000

    Add delete and update for actions

--- data/routers/action-routers.js
+++ data/routers/action-routers.js
@@ -37,8 +37,55 @@ router.post("/api/actions", (req, res) => {
   }
 });
 
-router.delete("/", (req, res) => {});
+router.delete("/api/actions/:id", (req, res) => {
+  actions
+    .remove(req.params.id)
+    .then(code => {
+      if (code === 1) {
+        res.status(202).json(`The action has been successfully deleted`);
+      } else {
+        res.status(404).json(`There isn't a action with this id`);
+      }
+    })
+    .catch(error => {
+      res.status(500).json(`There was an error deleting this action`);
+    });
+});
 
-router.put("/", (req, res) => {});
+router.put("/api/actions", (req, res) => {
+  if (
+    req.body.id &&
+    req.body.project_id &&
+    req.body.description &&
+    req.body.notes &&
+    req.body.completed
+  ) {
+    actions
+      .get(req.body.id)
+      .then(action => {
+        if (action === null) {
+          res.status(404).json(`There isn't an action with this id`);
+        } else {
+          actions
+            .update(req.body.id, req.body)
+            .then(action => {
+              res.status(200).json(action);
+            })
+            .catch(error => {
+              res.status(500).json(`There was an error updating this action`);
+            });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(`There was an error collecting actions data`);
+      });
+  } else {
+    res
+      .status(400)
+      .json(
+        "Please provide a project id, and the changes, including: a description, notes, and completed status for your action"
+      );
+  }
+});
 
 module.exports = router;
